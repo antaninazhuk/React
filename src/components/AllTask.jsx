01b2be068f3ff8ac3685a659f962950dc9583233
antaninazhuk <101988172+antaninazhuk@users.noreply.github.com>
2022-08-08 13:03:22 +0300

    Add files via upload

---
+++ src/components/AllTask.jsx
@@ -0,0 +1,10 @@
+function AllTask() {
+    return (
+    <div  className='AllTask'>
+   <span>You have 3 tasks </span>
+   <button>Delete All</button>
+    </div>
+    )
+  }
+  
+  export default AllTask;
---
+++ src/components/Task.jsx
@@ -0,0 +1,12 @@
+function Task() {
+    return (
+    <div className="Task">
+     <span>My Task</span>
+     
+
+      <button>Delete</button>
+    </div>
+    )
+  }
+  
+  export default Task;
